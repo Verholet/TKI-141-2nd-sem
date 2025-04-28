57decd91be001ef9450df612b6f7fd43d858ac42
Verholet <alex.verholet@icloud.com>
2025-04-28 12:10:00 +0300

    решение задания 1

---
+++ laba 1/main.cpp/main.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <string>
+#include "Person.h"
+
+int main(void)
+{
+	Person fio;
+	fio.show("Verholetov", "Alex");
+	fio.showFormal("Verholetov", "Alex", "Andreevich");
+}
