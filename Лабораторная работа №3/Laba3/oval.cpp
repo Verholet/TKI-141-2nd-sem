b9f97804b0032403c7d2f4ff917981308937db4f
Verholet <alex.verholet@icloud.com>
2025-06-16 09:57:28 +0300

    Task3(update)

--- Лабораторная работа №3/Laba3/oval.cpp
+++ Лабораторная работа №3/Laba3/oval.cpp
@@ -1,47 +1,82 @@
 #include <iostream>
-#include "oval.h"
+#include <string>
 #include <sstream>
-using namespace std;
+#include "point.h"
+#include "oval.h"
+
 Oval::Oval(int x1, int y1, int x2, int y2, int height, int width) {
-    height_ = height;
-    width_ = width;
-    if (x1 < 0 or y1 < 0 or x2 < 0 or y2 < 0 or height < y1 or height < y2 or width < x1 or width < x2) {
-        throw "Oval can't be created";
+    if (height <= 0 || width <= 0) {
+        throw std::invalid_argument("Height and width must be positive");
     }
+    if (x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0 ||
+        height < y1 || height < y2 || width < x1 || width < x2) {
+        throw std::invalid_argument("Points are out of bounds for the oval");
+    }
+
     f1 = Point(x1, y1);
     f2 = Point(x2, y2);
-
-
+    height_ = height;
+    width_ = width;
 }
+
 Oval::Oval(const Point& f1_, const Point& f2_, int height, int width) {
-    height_ = height;
-    width_ = width;
-    set_points(f1_, f2_);
-}
-string Oval::ToString() const {
-    ostringstream out;
-    out << "f1 =" << f1 << ", " << "f2 =" << f2;
-    return out.str();
-}
-Oval::Oval(int height, int width) {
-    height_ = height;
-    width_ = width;
-
-}
-void Oval::set_points(const Point& f1_, const Point& f2_) {
-    if (f1_.x() < 0 or f1_.y() < 0 or f2_.x() < 0 or f2_.y() < 0 or height_ < f1_.y() or height_ < f2_.y() or width_ < f1_.x() or width_ < f2_.x()) {
-        throw "Oval can't be created";
+    if (height <= 0 || width <= 0) {
+        throw std::invalid_argument("Height and width must be positive");
     }
+    if (f1_.x() < 0 || f1_.y() < 0 || f2_.x() < 0 || f2_.y() < 0 ||
+        height < f1_.y() || height < f2_.y() || width < f1_.x() || width < f2_.x()) {
+        throw std::invalid_argument("Points are out of bounds for the oval");
+    }
+
+    f1 = f1_;
+    f2 = f2_;
+    height_ = height;
+    width_ = width;
+}
+
+Oval::Oval(int height, int width) {
+    if (height <= 0 || width <= 0) {
+        throw std::invalid_argument("Height and width must be positive");
+    }
+
+    f1 = Point(0, 0);
+    f2 = Point(width, height);
+    height_ = height;
+    width_ = width;
+}
+
+void Oval::set_points(const Point& f1_, const Point& f2_) {
+    if (f1_.x() < 0 || f1_.y() < 0 || f2_.x() < 0 || f2_.y() < 0 ||
+        height_ < f1_.y() || height_ < f2_.y() || width_ < f1_.x() || width_ < f2_.x()) {
+        throw std::invalid_argument("Points are out of bounds for the oval");
+    }
+
     f1 = f1_;
     f2 = f2_;
 }
+
+string Oval::ToString() const {
+    ostringstream out;
+    out << "f1 = " << f1 << ", f2 = " << f2;
+    return out.str();
+}
+
 istream& operator>>(istream& in, Oval& other) {
     Point f1;
     Point f2;
-    cout << "ENTER f1 ";
+
+    cout << "ENTER f1: ";
     in >> f1;
-    cout << "ENTER f2 ";
+    cout << "ENTER f2: ";
     in >> f2;
-    other.set_points(f1, f2);
+
+    try {
+        other.set_points(f1, f2);
+    }
+    catch (const std::exception& e) {
+        cerr << "Error: " << e.what() << endl;
+        throw;
+    }
+
     return in;
 }
--- Лабораторная работа №3/Laba3/plane_shape.h
+++ Лабораторная работа №3/Laba3/plane_shape.h
@@ -1,11 +1,10 @@
 #pragma once
 #include <iostream>
 #include "point.h"
-using namespace std;;
+
 class PlaneShape {
 public:
-	virtual string ToString() const = 0;
-	void draw() const;
-	~PlaneShape();
-
+    virtual string ToString() const = 0;
+    void draw() const;
+    virtual ~PlaneShape() {}
 };
